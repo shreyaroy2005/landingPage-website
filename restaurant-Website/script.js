2ebb2948adaef2ba30b65cba25c3bb41c7b5c87d
shreya Roy <shreyaroy1945@gmail.com>
2026-07-14 13:53:18 +0530

    add all updates files

--- restaurant-Website/script.js
+++ restaurant-Website/script.js
@@ -14,3 +14,17 @@ themeToggle.addEventListener("click", () => {
         themefoodi.src = "./image/drakFoodi.png";
     }
 });
+
+
+const navLinks = document.querySelectorAll(".navlink li a");
+const currentUrl = window.location.pathname;
+
+navLinks.forEach((link) => {
+    // link.getAttribute("href") = tag tar href er value ta ki.
+    // check korche currentUrl ar modhe href er nam ta ache kina dekhche.
+    if (currentUrl.includes(link.getAttribute("href"))) {
+        link.classList.add("active");
+    }else{
+        link.classList.remove("active");
+    }
+})
